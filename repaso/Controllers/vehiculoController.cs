4eefdb5d02ca6a4bc18f07c1962e55323f61e928
Cristian Salazar <cristian192003@outlook.es>
2021-05-28 14:58:10 -0500

    programa Terminado

---
+++ repaso/Controllers/vehiculoController.cs
@@ -0,0 +1,67 @@
+﻿using repaso.Dato;
+using repaso.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace repaso.Controllers
+{
+    public class vehiculoController : Controller
+    {
+        vehiculoAdmin admin = new vehiculoAdmin();
+        // GET: vehiculo
+        public ActionResult Index()
+        {
+            IEnumerable<vehiculo> lista = admin.Consultar();
+            return View(lista);
+        }
+
+        public ActionResult Guardar()
+        {
+            ViewBag.mensaje = "";
+            return View();
+        }
+        public ActionResult Nuevo(vehiculo modelo)
+        {
+            admin.Guardar(modelo);
+            ViewBag.mensaje = "Vehiculo Guardado";
+            return View("Guardar", modelo);
+        }
+
+        public ActionResult Detalle(int id = 0)
+        {
+            vehiculo modelo = admin.Consultar(id);
+            return View(modelo);
+        }
+
+        public ActionResult Modificar(int id = 0)
+        {
+            ViewBag.mensaje = "";
+            vehiculo modelo = admin.Consultar(id);
+            return View(modelo);
+        }
+
+        public ActionResult Actualizar(vehiculo modelo)
+        {
+            admin.Modificar(modelo);
+            ViewBag.mensaje = "Vehiculo Actualizado";
+            return View("Modificar", modelo);
+        }
+
+        public ActionResult Eliminar( int id = 0)
+        {
+            vehiculo modelo = new vehiculo()
+            {
+                Id = id
+            };
+            admin.Eliminar(modelo);
+            IEnumerable<vehiculo> lista = admin.Consultar();
+            ViewBag.mensaje = "Vehiculo Eliminado";
+            return View("Index", lista);
+        }
+    }
+
+    
+}
---
+++ repaso/Dato/vehiculoAdmin.cs
@@ -0,0 +1,60 @@
+﻿using repaso.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace repaso.Dato
+{
+    public class vehiculoAdmin
+    {
+        //Este es para consultar datos
+        public IEnumerable<vehiculo> Consultar()
+        {
+            using(autosEntities contexto = new autosEntities())
+            {
+                return contexto.vehiculo.AsNoTracking().ToList();
+            }
+        }
+
+        //Guardar vehiculo en la base de datos
+        public void Guardar(vehiculo modelo)
+        {
+            using(autosEntities contexto = new autosEntities())
+            {
+                contexto.vehiculo.Add(modelo);//Aqui agrego lo que me manden de la pagina
+                contexto.SaveChanges();
+            }
+        }
+
+        //Consultar
+        public vehiculo Consultar(int id)
+        {
+            using(autosEntities contexto = new autosEntities())
+            {
+                return contexto.vehiculo.FirstOrDefault(v => v.Id == id);
+            }
+        }
+
+        //Modificar los datos del vehiculo
+        public void Modificar(vehiculo modelo)
+        {
+            using (autosEntities contexto = new autosEntities())
+            {
+                contexto.Entry(modelo).State = System.Data.Entity.EntityState.Modified;
+                contexto.SaveChanges();
+            }
+        }
+
+
+        //Eliminar auto
+        public void Eliminar(vehiculo modelo)
+        {
+            using(autosEntities contexto = new autosEntities())
+            {
+                contexto.Entry(modelo).State = System.Data.Entity.EntityState.Deleted;
+                contexto.SaveChanges();
+            }
+        }
+    }
+}
